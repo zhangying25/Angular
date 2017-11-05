892f127d33a217e229c5d6449b14758939aeb058
Ying Zhang <zhang_ying@msn.com>
2017-11-05 15:22:00 -0800

    observable

--- conFusion/src/app/services/dish.service.ts
+++ conFusion/src/app/services/dish.service.ts
@@ -20,7 +20,11 @@ export class DishService {
   	return Observable.of(DISHES.filter((dish) => (dish.id === id))[0]).delay(2000);
   }
 
-  getFeaturedDish(): Observable<Dish> {
-  	return Observable.of(DISHES.filter((dish) => (dish.featured === true))[0]).delay(2000);
+  getFeaturedDish(): Promise<Dish> {
+  	return Promise.resolve(DISHES.filter((dish) => (dish.featured === true))[0]);
+  }
+
+  getDishIds(): Observable<number[]> {
+    return Observable.of(DISHES.map(dish => dish.id)).delay(2000);
   }
 }
