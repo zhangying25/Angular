fc9757d9d289bc252c4456164031e1c3187d5427
Ying Zhang <yingz@ebay.com>
2017-11-18 12:38:49 -0800

    animation 2

--- conFusion/src/app/dishdetail/dishdetail.component.ts
+++ conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -8,8 +8,7 @@ import { DishService } from '../services/dish.service';
 
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Comment } from '../shared/comment';
-
-import { trigger, state, style, animate, transition } from '@angular/animations';
+import { visibility, flyInOut, expand } from '../animations/app.animation';
 
 import 'rxjs/add/operator/switchMap';
 
@@ -17,18 +16,14 @@ import 'rxjs/add/operator/switchMap';
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
   styleUrls: ['./dishdetail.component.scss'],
+  host: {
+    "[@flyInOut]": 'true',
+    'style': 'display: block;'
+  },
   animations: [
-    trigger('visibility', [
-        state('shown', style({
-          transform: 'scale(1.0)',
-          opacity: 1
-        })),
-        state('hidden', style({
-          transform: 'scale(0.5)',
-          opacity: 0
-        })),
-        transition('* => *', animate('0.5s ease-in-out'))
-      ])
+    flyInOut(),
+    visibility(),
+    expand()
   ]
 })
 export class DishdetailComponent implements OnInit {
